b29562957e06aaf45f84c38462a58485b4c26ec8
Rohitwable <rohitwable46@gmail.com>
2019-09-25 17:06:53 +0530

    initial server set up

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, err => {
+    if(err) throw err;
+    console.log(`Server is running on port ${port}`);
+})
